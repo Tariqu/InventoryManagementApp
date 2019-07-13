0d2346b4dfc9c3c0f36b4601301e57e5201a2a44
Tarique Akhtar Ansari <tarique.rkl@gmail.com>
2019-07-13 18:40:42 +0530

    Update config.js

--- api/config/config.js
+++ api/config/config.js
@@ -2,5 +2,5 @@ module.exports = {
   PORT: 5000,
   SCERET_KEY: "asdfasdf",
   DB_URL:
-    "mongodb+srv://babaji123:babaji123@cluster0-o7ddq.mongodb.net/test?retryWrites=true&w=majority"
+    "mongodb+srv://<usedid>:<password>@cluster0-o7ddq.mongodb.net/test?retryWrites=true&w=majority"
 };
