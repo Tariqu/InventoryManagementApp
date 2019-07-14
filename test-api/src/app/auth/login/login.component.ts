03a2ffe38f7183bc4fe311adeb868aebcc394303
Tarique Akhtar Ansari <tarique.rkl@gmail.com>
2019-07-14 10:13:12 +0530

    "Blank" Validation for login and signup page

--- test-api/src/app/auth/login/login.component.ts
+++ test-api/src/app/auth/login/login.component.ts
@@ -9,22 +9,28 @@ import { DashboardService } from "src/app/dashboard/dashboard.service";
   styleUrls: ["./login.component.css"]
 })
 export class LoginComponent implements OnInit {
-  username;
-  password;
+  username = "";
+  password = "";
   constructor(private auth: AuthService, private router: Router) {}
 
   ngOnInit() {}
   login() {
-    this.auth
-      .login({ username: this.username, password: this.password })
-      .subscribe(result => {
-        if (result.success) {
-          localStorage.setItem("token", result.token);
-          this.router.navigate(["/dashboard"]);
-        } else {
-          this.auth.openSnackBar(result.message, "ok");
-        }
-      });
+    if (this.username === "") {
+      this.auth.openSnackBar("Please Enter Username...", "ok");
+    } else if (this.password === "") {
+      this.auth.openSnackBar("Please Enter Password...", "ok");
+    } else {
+      this.auth
+        .login({ username: this.username, password: this.password })
+        .subscribe(result => {
+          if (result.success) {
+            localStorage.setItem("token", result.token);
+            this.router.navigate(["/dashboard"]);
+          } else {
+            this.auth.openSnackBar(result.message, "ok");
+          }
+        });
+    }
   }
   navigate() {
     this.router.navigate(["/signup"]);
--- test-api/src/app/auth/signup/signup.component.ts
+++ test-api/src/app/auth/signup/signup.component.ts
@@ -14,25 +14,33 @@ export interface Response {
   styleUrls: ["./signup.component.css"]
 })
 export class SignupComponent implements OnInit {
-  username;
-  email;
-  password;
+  username = "";
+  email = "";
+  password = "";
   constructor(private authService: AuthService, private router: Router) {}
 
   ngOnInit() {}
   signupProcess() {
-    const formValue = {
-      username: this.username,
-      email: this.email,
-      password: this.password
-    };
-    this.authService.signup(formValue).subscribe((result: any) => {
-      if (result.success) {
-        this.router.navigate(["/login"]);
-      } else {
-        this.authService.openSnackBar(result.message, "ok");
-      }
-    });
+    if (this.username === "") {
+      this.authService.openSnackBar("Please Enter Username...", "ok");
+    } else if (this.email === "") {
+      this.authService.openSnackBar("Please Enter Email...", "ok");
+    } else if (this.password === "") {
+      this.authService.openSnackBar("Please Enter Password...", "ok");
+    } else {
+      const formValue = {
+        username: this.username,
+        email: this.email,
+        password: this.password
+      };
+      this.authService.signup(formValue).subscribe((result: any) => {
+        if (result.success) {
+          this.navigate();
+        } else {
+          this.authService.openSnackBar(result.message, "ok");
+        }
+      });
+    }
   }
   navigate() {
     this.router.navigate(["/login"]);
