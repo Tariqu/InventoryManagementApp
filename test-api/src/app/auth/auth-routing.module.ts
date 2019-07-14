28e755fef8af661abdff61983f20cb5b2db426e0
Tarique Akhtar Ansari <tarique.rkl@gmail.com>
2019-07-14 09:17:53 +0530

    path redirected to login

--- test-api/src/app/auth/auth-routing.module.ts
+++ test-api/src/app/auth/auth-routing.module.ts
@@ -1,16 +1,21 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
-import { LoginComponent } from './login/login.component';
-import { SignupComponent } from './signup/signup.component';
+import { NgModule } from "@angular/core";
+import { Routes, RouterModule } from "@angular/router";
+import { LoginComponent } from "./login/login.component";
+import { SignupComponent } from "./signup/signup.component";
 
 const routes: Routes = [
   {
-    path: 'login',
+    path: "login",
     component: LoginComponent
   },
   {
-    path: 'signup',
+    path: "signup",
     component: SignupComponent
+  },
+  {
+    path: "",
+    redirectTo: "/login",
+    pathMatch: "full"
   }
 ];
 
